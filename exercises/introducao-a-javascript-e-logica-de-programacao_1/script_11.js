467dfbbeaec3fd3bb07574c30cf96dc95452cfba
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-11-26 02:03:49 -0300

    C6 --> Programa 11 feito.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script_11.js
@@ -0,0 +1,23 @@
+let grade = 40;
+
+if (grade < 0 || grade > 100) {
+    console.log ('Error. declared grade is not between 0 and 100.');
+}
+else if (grade >= 90) {
+    console.log ('A');
+}
+else if (grade >= 80) {
+    console.log ('B');
+}
+else if (grade >= 70) {
+    console.log ('C');
+}
+else if (grade >= 60) {
+    console.log ('D');
+}    
+else if (grade >= 50) {
+    console.log ('E');
+}
+else {
+    console.log ('F');
+}
