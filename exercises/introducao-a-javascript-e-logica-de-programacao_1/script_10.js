da853f5e1be73ba81e69e99dd09c63d0ba28cb84
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-11-26 17:08:35 -0300

    C11 --> script_10 otimizado

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script_10.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script_10.js
@@ -1,7 +1,17 @@
 let grossPay = 3500;
 
+let trybePayment = 0;
 let inssTax;
 let irTax;
+
+console.log ('Gross Salary: ' + grossPay.toFixed(2));
+
+if (grossPay >= 3500) {
+    trybePayment = grossPay * 0.17;
+}
+
+console.log ('Trybe: R$ ' + trybePayment.toFixed(2));
+grossPay = grossPay - trybePayment;
 
 if (grossPay <= 1556.94) {
     inssTax = grossPay * 0.08;
@@ -16,22 +26,28 @@ else {
     inssTax = 570.88;
 }
 
-let netPay = grossPay - inssTax;
+console.log ('INSS: R$ ' + inssTax.toFixed(2));
+grossPay = grossPay - inssTax;
 
-if (netPay <= 1903.98) {
+if (grossPay <= 1903.98) {
     irTax = 0;
 }
-else if (netPay <= 2826.65) {
-    irTax = netPay * 0.075 - 142.80;
+else if (grossPay <= 2826.65) {
+    irTax = grossPay * 0.075 - 142.80;
 }
-else if (netPay <= 3751.05) {
-    irTax = netPay * 0.15 - 354.80;
+else if (grossPay <= 3751.05) {
+    irTax = grossPay * 0.15 - 354.80;
 }
-else if (netPay <= 4664.68) {
-    irTax = netPay * 0.225 - 636.13;
+else if (grossPay <= 4664.68) {
+    irTax = grossPay * 0.225 - 636.13;
 }
 else {
-    irTax = netPay * 0.275 - 869.36;
+    irTax = grossPay * 0.275 - 869.36;
 }
 
-console.log ('Salary: ' + (netPay - irTax));
+let netPay = grossPay - irTax;
+let totalTax = trybePayment + inssTax + irTax;
+
+console.log ('IR: R$ ' + irTax.toFixed(2));
+console.log ('Total Tax: R$ ' + totalTax.toFixed(2));
+console.log ('Net Salary: R$ ' + netPay.toFixed(2));
