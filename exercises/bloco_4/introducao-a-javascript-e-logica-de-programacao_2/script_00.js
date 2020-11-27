82f667d354dedc6b25ab1ab470067cf822343dab
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-11-26 22:35:23 -0300

    C01 --> Exercicio 1 corrigido.

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/script_00.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/script_00.js
@@ -1,3 +1,8 @@
+//Exercício 01:
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log (numbers);
+console.log ()
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log (numbers[index]);
+}
