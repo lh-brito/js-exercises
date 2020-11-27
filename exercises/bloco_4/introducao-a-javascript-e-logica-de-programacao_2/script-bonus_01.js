0a73abc76364c8c59ab2c8a68d006c5a5394103b
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-11-27 03:45:57 -0300

    C06 --> Exercicios Bonus 1, 2 e 3 feitos.

---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/script-bonus_01.js
@@ -0,0 +1,16 @@
+//Exercício Bônus 02:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let indexOne = 1; indexOne < numbers.length; indexOne += 1) {
+    for (let indexTwo = 0; indexTwo < indexOne; indexTwo += 1) {
+        if (numbers[indexOne] > numbers[indexTwo]) {
+            let position = numbers[indexOne];
+            
+            numbers[indexOne] = numbers[indexTwo];
+            numbers[indexTwo] = position;
+        }
+    }
+}
+
+console.log (numbers);
---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/script-bonus_02.js
@@ -0,0 +1,16 @@
+//Exercício Bônus 03:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let multiplier = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (index !== numbers.length - 1) {
+        multiplier.push(numbers[index] * numbers[index + 1]);
+    }
+    else {
+        multiplier.push(numbers[index] * 2)
+    }
+}
+
+console.log (multiplier);
