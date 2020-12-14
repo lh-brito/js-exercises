0c12c2b03b801455a35d1dbc744edccec7c9c71a
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2020-12-14 02:52:23 -0300

    Todos exercicios do dia 5.2 feitos.

--- exercises/bloco_5/dia_2/exercicios_conteudo/script.js
+++ exercises/bloco_5/dia_2/exercicios_conteudo/script.js
@@ -1,24 +1,64 @@
- // 1 - Acesse o elemento elementoOndeVoceEsta.
- document.getElementById("elementoOndeVoceEsta");
-        
- // 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
- document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "red";
+// Exercícios: Parte I - Buscando elementos
 
- // 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
- document.getElementById("primeiroFilhoDoFilho").innerText = "texto de teste";
+// 1 - Acesse o elemento elementoOndeVoceEsta.
+document.getElementById("elementoOndeVoceEsta");
+    
+// 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+document.getElementsByTagName("div")[3].parentNode.style.color = "blue";
 
- // 4 - Acesse o primeiroFilho a partir de pai.
- document.getElementById("pai").childNodes[1];
+// 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
+document.querySelector("#primeiroFilhoDoFilho").innerText = "Insira algum texto inteligente aqui\n\n";
 
- // 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
- document.getElementById("elementoOndeVoceEsta").previousElementSibling;
+// 4 - Acesse o primeiroFilho a partir de pai.
+document.querySelectorAll("#pai")[0].childNodes[1];
 
- // 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
- document.getElementById("elementoOndeVoceEsta").nextSibling;
+// 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+document.getElementById("elementoOndeVoceEsta").previousElementSibling;
 
- // 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
- document.getElementById("elementoOndeVoceEsta").nextElementSibling;
+// 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+document.getElementsByTagName("div")[3].nextSibling;
 
- // 8 - Agora acesse o terceiroFilho a partir de pai.
- document.getElementById("pai").childNodes[5];
- 
+// 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+document.querySelector("#elementoOndeVoceEsta").nextElementSibling;
+
+// 8 - Agora acesse o terceiroFilho a partir de pai.
+document.querySelectorAll("#pai")[0].childNodes[5];
+
+// Exercícios: Parte II - Criando elementos
+
+// 1 - Crie um irmão para elementoOndeVoceEsta.
+let anotherBrother = document.createElement("div");
+anotherBrother.innerText = "\nOutra(o) irmã(o).\n'Elemento Div irmão do Div com ID (elementoOndeVoceEsta).'";
+anotherBrother.id = "quintoEMaisNovoFilho";
+document.getElementById("pai").appendChild(anotherBrother);
+
+// 2 - Crie um filho para elementoOndeVoceEsta.
+let myNewChild = document.createElement("div");
+myNewChild.innerText = "Meu novo filho.\n'Elemento Div filho do Div com ID (elementoOndeVoceEsta).'\n\n";
+myNewChild.id = "terceiroEMaisNovoFilhoDoFilho";
+document.getElementsByTagName("div")[3].appendChild(myNewChild);
+
+// 3 - Crie um filho para primeiroFilhoDoFilho.
+let childOfMyChild = document.createElement("div");
+childOfMyChild.innerText = "Filha do meu filho.\n'Elemento Div filho do Div com ID (primeiroFilhoDoFilho).'\n\n"
+childOfMyChild.id = "filhoDoFilhoDoFilho";
+document.querySelector("#primeiroFilhoDoFilho").appendChild(childOfMyChild);
+
+// 4 - A partir desse filho criado, acesse terceiroFilho.
+document.querySelectorAll("#filhoDoFilhoDoFilho")[0].parentNode.parentNode.nextElementSibling;
+
+// Exercícios: Parte III - Removendo elementos
+
+// 1 - Remova todos os elementos da página, menos pai, elementoOndeVoceEsta e primeiroFilhoDoFilho.
+let father = document.getElementById('pai');
+
+father.firstElementChild.remove();
+for (let i = 0; i < 3; i += 1) {
+    father.lastElementChild.remove();
+}
+
+let son = father.firstElementChild;
+
+son.lastElementChild.remove();
+son.lastElementChild.remove();
+son.firstElementChild.lastElementChild.remove();
